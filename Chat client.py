f3cbe78507602aeb7c797b854e32f5339c76e70c
Alireza <alirezasaadat056@gamil.com>
2024-05-04 01:20:09 -0700

    initial commit

---
+++ Chat client.py
@@ -0,0 +1,94 @@
+import tkinter as tk
+from tkinter import messagebox
+import socket
+import threading
+
+PORT = 7070
+FORMAT = "utf_8"
+HEADER = 64
+SERVER = socket.gethostbyname(socket.gethostname())
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER, PORT))
+
+class ChatApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Chat GUI")
+        self.root.geometry("500x500")
+        
+        self.name_lb = tk.Label(root, text="Enter your name:")
+        self.name_lb.pack(pady=15)
+        self.name_entry = tk.Entry(root)
+        self.name_entry.pack(pady=15)
+        self.btn1 = tk.Button(root, text="Submit", command=self.set_name)
+        self.btn1.pack()
+        
+        self.name = ""
+        self.made = False
+        
+        self.thread = threading.Thread(target=self.receive_messages)
+        self.thread.start()
+
+    def set_name(self):
+        name = self.name_entry.get()
+        if name:
+            self.name += name
+            self.create_gui()
+            self.send_message(self.name)
+            self.name_lb.destroy()
+            self.name_entry.destroy()
+            self.btn1.destroy()
+
+    def create_gui(self):
+        if not self.made:
+            self.frame = tk.Frame(self.root)
+            self.frame.pack(pady=10)
+            self.chat_list = tk.Listbox(self.frame, width=50, height=20)
+            self.chat_list.pack(side=tk.LEFT)
+            self.scrollbar = tk.Scrollbar(self.frame, orient=tk.VERTICAL, command=self.chat_list.yview)
+            self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+            self.chat_list.config(yscrollcommand=self.scrollbar.set)
+            self.entry = tk.Entry(self.root, width=40)
+            self.entry.pack(pady=10)
+            self.send_button = tk.Button(self.root, text="Send", command=self.send)
+            self.send_button.pack()
+            self.made = True
+
+    def receive_messages(self):
+        while True:
+            try:
+                msg = client.recv(2048).decode(FORMAT)
+                name, msg = msg.split(" ", 1)
+                self.root.after(0, self.update_chat, name, msg)
+            except Exception as e:
+                print(e)
+                break
+
+    def send(self):
+        message = self.entry.get()
+        if message and self.name:
+            self.chat_list.insert(tk.END, "You: " + message)
+            self.send_message(message)
+            self.entry.delete(0, tk.END)
+
+    def send_message(self, message):
+        message = message.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b" " * (HEADER - len(send_length))
+        client.send((send_length))
+        client.send(message)
+
+    def update_chat(self, name, message):
+        if name == self.name:
+            self.chat_list.insert(tk.END, "You: " + message)
+        else:
+            self.chat_list.insert(tk.END, f"{name}: " + message)
+
+def main():
+    root = tk.Tk()
+    app = ChatApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ Chat server.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+
+
+PORT = 7070
+
+FORMAT = "utf_8"
+
+HEADER = 64
+
+SERVER = socket.gethostbyname(socket.gethostname())
+
+clients = {}
+
+server = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+
+server.bind((SERVER , PORT))
+
+def handle_client(connection , address):
+    print(f"NEW CONNECTION {address}")
+    clients[connection] = ""
+    
+    while True:
+        masg = connection.recv(HEADER).decode(FORMAT)
+        if masg:
+            masg = int(masg)
+            masg = connection.recv(masg).decode(FORMAT)
+            if masg == "disconnect":
+                break
+            if clients[connection] == "":
+                clients[connection] = masg
+                continue
+            for client in clients.keys():
+                if client != connection:
+                    client.send(f"{clients[connection]} {masg}".encode(FORMAT))
+    connection.close()
+        
+
+def start():
+    print("[SERVER IS RUNNING] server is starting... ")
+    server.listen()
+    while True:   
+        conection , address = server.accept()
+        thread = threading.Thread(target=handle_client , args=(conection , address))
+        thread.start()
+        print(f"ACTiVE CONNECTIONS =====> {threading.active_count() - 1} \n")
+
+start()
+
